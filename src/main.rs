dfeb429b8e68056174839219f8c703f7fa5accf9
DB-Vincent <vincent.deborger@gmail.com>
2022-09-25 20:33:20 +0200

    Implement sorting the command's output

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 mod utils;
 
+use std::cmp::Reverse;
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::{Path};
 use std::error::Error;
 use std::process;
 use std::os::unix::fs::PermissionsExt;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use chrono::{DateTime, Local};
 
 /// A *very* simple improvement on the already great ls-command.
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Opts {
     /// Path to the directory
@@ -25,26 +26,51 @@ struct Opts {
     /// Show only files
     #[clap(short, long)]
     files_only: bool,
+    /// Sort the resulting files and/or direcories in a specific way
+    #[clap(short, long, value_enum, default_value="name")]
+    sort: SortingKey,
+    /// Reverse the sorting result
+    #[clap(short, long)]
+    reverse: bool,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum SortingKey {
+    Size,
+    Name,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
 
-    if let Err(ref e) = run(Path::new(&opts.path), opts.all, opts.dirs_only, opts.files_only) {
+    if let Err(ref e) = run(opts) {
         println!("{}", e);
         process::exit(1);
     }
 }
 
-fn run(dir: &Path, all: bool, dirs_only: bool, files_only: bool) -> Result<(), Box<dyn Error>> {
+fn run(opts: Opts) -> Result<(), Box<dyn Error>> {
+    let dir = Path::new(&opts.path);
+
     if dir.is_dir() {
-        let files: Result<Vec<PathBuf>, Box<dyn Error>>= Ok(fs::read_dir(dir)?
+        let mut files: Vec<_> = fs::read_dir(dir).unwrap()
             .into_iter()
             .filter(|r| r.is_ok()) // Get rid of Err variants for Result<DirEntry>
             .map(|r| r.unwrap().path()) // This is safe, since we only have the Ok variants
-            .collect());
+            .collect();
 
-        for entry in files? {
+        match opts.sort {
+            SortingKey::Size => {
+                if opts.reverse { files.sort_by_key(|file| Reverse(file.metadata().expect("REASON").len())); }
+                else { files.sort_by_key(|file| file.metadata().expect("REASON").len()); }
+            }
+            SortingKey::Name => {
+                if opts.reverse { files.sort_by_key(|file| Reverse(file.to_path_buf())); }
+                else { files.sort_by_key(|file| file.to_path_buf()); }
+            }
+        }
+
+        for entry in files {
             let metadata = entry.metadata()?;
             let file_name: &str = entry
                 .as_path()
@@ -56,7 +82,7 @@ fn run(dir: &Path, all: bool, dirs_only: bool, files_only: bool) -> Result<(), B
             let modified: DateTime<Local> = DateTime::from(metadata.modified()?);
             let mode = metadata.permissions().mode();
 
-            if (all || !file_name.starts_with('.')) && (!dirs_only || entry.is_dir()) && (!files_only || !entry.is_dir()) {
+            if (opts.all || !file_name.starts_with('.')) && (!opts.dirs_only || entry.is_dir()) && (!opts.files_only || !entry.is_dir()) {
                 println!(
                     "{}   {}   {:>9}   {}",
                     utils::parse_permissions(mode as u32),
